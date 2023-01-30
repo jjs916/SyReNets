83d1705239ac7c497afa605c0cf208d572fc16dd
jjs916 <57747577+jjs916@users.noreply.github.com>
2023-01-30 15:15:01 +0100

    Update train.py
    different args

--- syrenets_code/train.py
+++ syrenets_code/train.py
@@ -7,11 +7,11 @@ msg = 'train a model in a given experiment modality'
 # initialize parser
 parser = argparse.ArgumentParser(description=msg)
 parser.add_argument('experiment', help='experiment type in string form (\'direct\' or \'indirect\')')
-parser.add_argument('-ni','--number_inputs', help='number of input to the model (q, dq, ddq)', default=6)
-parser.add_argument('-d','--depth', help='number of symbolic layers in the network', default=3)
-parser.add_argument('-sl','--number_selection_heads', help='number of selection heads in each symbolic layer', default=12)
-parser.add_argument('-mb','--number_mini_match', help='number of mini batches in training', default=1000)
-parser.add_argument('-ns','--number_samples', help='number of samples in each mini match', default=32)
+parser.add_argument('-ni','--number_inputs', type=int, help='number of input to the model (q, dq, ddq)', default=6)
+parser.add_argument('-d','--depth', type=int, help='number of symbolic layers in the network', default=3)
+parser.add_argument('-sl','--number_selection_heads', type=int, help='number of selection heads in each symbolic layer', default=12)
+parser.add_argument('-mb','--number_mini_match', type=int, help='number of mini batches in training', default=1000)
+parser.add_argument('-ns','--number_samples', type=int, help='number of samples in each mini match', default=32)
 args = parser.parse_args()
 
 train(args.experiment, n_inp=args.number_inputs, depth=args.depth, n_sel=args.number_selection_heads, \
