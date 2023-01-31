efdda9466c9b9c29fea1a10f5c2f8033cbb1fbd8
Philipp Scholl <scholl@math.lmu.de>
2023-01-31 16:21:59 +0100

    intermediary results

--- syrenets_code/train_syrenet.sh
+++ syrenets_code/train_syrenet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python train.py math2 -sl 2 -d 2 -niter 1 -lambda_2 0.001
-python train.py math2 -sl 2 -d 2 -niter 1 -lambda_2 0.01
-python train.py math2 -sl 2 -d 2 -niter 1 -lambda_2 0.1
-python train.py math2 -sl 2 -d 2 -niter 1 -lambda_2 1
+python train.py math3 -sl 4 -d 4 -niter 10000 -lambda_2 0.001
+python train.py math3 -sl 4 -d 4 -niter 10000 -lambda_2 0.01
+python train.py math3 -sl 4 -d 4 -niter 10000 -lambda_2 0.1
+python train.py math3 -sl 4 -d 4 -niter 10000 -lambda_2 1
